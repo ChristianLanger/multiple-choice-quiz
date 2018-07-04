f31f1fd7a83c55edf9ed752917c5141ddf7c9317
Christian Langer <christian.langer@idiv.de>
2018-07-04 14:54:29 +0200

    new files

---
+++ README.md
@@ -0,0 +1,13 @@
+## Multiple Choice Quiz 
+
+A simple dynamic Multiple Choice Quiz based on HTML/Javascript. Questions and answers are fetched from JSON file, so its easy to add more questions to the quiz if desired.
+
+Length: 5 questions
+
+
+![Screenshot1](https://home.uni-leipzig.de/idiv/quiz/screens/screen-3.png)
+
+![Screenshot2](https://home.uni-leipzig.de/idiv/quiz/screens/screen-2.png)
+
+![Screenshot3](https://home.uni-leipzig.de/idiv/quiz/screens/screen-1.png)
+
---
+++ js/questions.js
@@ -0,0 +1,67 @@
+const myQuestions = [
+  {
+    title: "Question 1 - Unusual Working Hours",
+    question: "If you plan to work on weekends, a prior written authorization by your supervisor is required and the H&S group coordinator should be notified (via whatsapp) if you are in the office on holidays or weekends. Please send him a short message... <p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/bg_slide1.jpg'>",
+    answers: {
+      a: "when you come to iDiv and go.",
+      b: "when you are supposed to be in iDiv.",
+      c: "no message required."
+    },
+    correctAnswer: "a",
+    correctAnswerText: "That is correct.<p>The H&S group coordinator should be notified if you are in the office on holidays or weekends. For safety reasons, please send him a short message when you come and go.</p>",
+    falseAnswerText: "That's not correct.<p>The iDiv H&S group coordinator should be informed when you come and go.</p>"
+  },
+  {
+    title: "Question 2 - Major Accidents",
+    question: "In the event of a serious accident which requires a doctor you must submit an accident report to your university and a copy to the iDiv H&S coordinator. Accident report must be filled in German. How many days after the accident you must submit the report?.<p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/bg_slide2.jpg'>",
+    answers: {
+      a: "Submit within 2 days of the accident",
+      b: "Submit within 3 days of the accident",
+      c: "Submit within 5 days of the accident"
+    },
+    correctAnswer: "b",
+    correctAnswerText: "That is correct.<p>Submit accident report to MLU within 3 days of the accident – send a copy to iDiv H&S coordinator (forms can be downloaded from the intranet).</p>",
+    falseAnswerText: "That is wrong.<p>Submit this form within 3 days of the accident!</p>"
+  },
+  {
+    title: "Question 3 - Outside the office",
+    question: "You plan to go to a field trip to Peneda Geres? Remember: A business trip form has to be submitted, including student assistants.<p>It's lunchtime, you plan to go to the HIT market to buy some food. Are you health insured inside HIT?</p><p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/bg_slide3.jpg'>",
+    answers: {
+      a: "Yes, full coverage is included.",
+      b: "No coverage through employer‘s health insurance.",
+      c: "No, the way TO HIT is not covered but inside HIT!"
+    },
+    correctAnswer: "b",
+    correctAnswerText: "That is correct.<p>No coverage through employer‘s health/liability insurance at HIT or cafeteria. The way TO HIT is covered, but not inside!</p>",
+    falseAnswerText: "That´s wrong!<p>You are not health insured inside HIT, only the way TO the supermarket is covered by your health insurance.</p>"
+  },
+  {
+    title: "Question 4 - Assembly point",
+    question: "Do you know our assembly point of Interim II?<p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/bg_slide4.gif'>",
+    answers: {
+      a: "X1",
+      b: "X2",
+      c: "X3"
+    },
+    correctAnswer: "a",
+    correctAnswerText: "That is correct.<p>Our assembly point is opposite the street under the trees.</p>",
+    falseAnswerText: "That is not correct.<p>X2 is no assembly point and X3 is the assembly point for Interim III.</p>"
+  },
+  {
+    title: "Question 5 - Fire alarm",
+    question: "Almost there...<p>In case of fire. Alarm & warn people in the building! Fire alarms are located in the hallways! Do you know the location of the fire alarms in our BIOCON hallway?</p><p>Select your answer then Confirm.</p>",
+    background: "<img src='./img/bg_slide5.jpg'>",
+    answers: {
+      a: "Main entrance door",
+      b: "Next to Resilience room",
+      c: "Opposite of woman restroom"
+    },
+    correctAnswer: "c",
+    correctAnswerText: "That is correct.<p>The fire alarm is located opposite of woman restroom. Call fire service - 112! Focus on main questions: WHO is calling? WHAT happened? WHERE did it happen? HOW many are injured? WAIT for questions!</p>",
+    falseAnswerText: "That is not correct.<p>Check the floor before you try again.</p>"
+  }
+];
